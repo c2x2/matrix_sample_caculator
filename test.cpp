745811f32079cbf05a7d898c0b81ec86d33fc605
c2x2 <1831045562@qq.com>
2023-08-10 11:22:05 +0800

    add + - = and input elements

--- test.cpp
+++ test.cpp
@@ -7,6 +7,10 @@ private:
 public:
     matrix(int m,int n);
     void print();
+    friend istream& operator>>(istream &in,matrix &m);// 重载>>运算符;
+    matrix& operator+(const matrix &m2);//重载加法
+    matrix& operator-(const matrix &m2);//重載减法
+    matrix &operator=(const matrix &m2); // 重载=
     ~matrix(){};
 }; // 定义矩阵类
 matrix::matrix(int m,int n){
@@ -29,8 +33,66 @@ void matrix::print(){
         cout<<endl;
     }
 }
+istream& operator>>(istream &in,matrix &m){
+    m.p = new int *[m.m];
+    for (int i = 0; i < m.m;i++){
+        m.p[i] = new int[m.n];
+    }
+    for (int i = 0; i < m.m;i++){
+        for (int j = 0; j < m.n;j++){
+            in>>m.p[i][j];
+        }
+    }
+    return in;
+}
+//定义+函数
+matrix& matrix::operator+(const matrix &m2){
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            p[i][j]=p[i][j]+m2.p[i][j];
+        }
+    }
+    return *this;
+}
+//定义-函数
+matrix& matrix::operator-(const matrix &m2){
+    
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){  
+            this->p[i][j]=this->p[i][j]-m2.p[i][j];
+        }
+    }
+    return *this;
+}
+//定义=函数
+matrix& matrix::operator=(const matrix &m2){
+    if(this==&m2){
+        return *this;
+    }
+    else{
+        delete []p;
+        m=m2.m;
+        n=m2.n;
+        p=new int*[m];
+        for(int i=0;i<m;i++){
+            p[i]=new int[n];
+            for(int j=0;j<n;j++){
+                p[i][j]=m2.p[i][j];
+            }
+        }
+        for (int i = 0; i < m;i++){
+            for (int j = 0; j < n;j++){
+                this->p[i][j] = m2.p[i][j];
+            }
+        }
+    }
+    return *this;
+}
 int main(){
     matrix m(3,3);
-    m.print();
+    cin>>m;
+    matrix m2(3,3);
+    cin>>m2;    
+    (m+m2).print();
     return 0;
 }
